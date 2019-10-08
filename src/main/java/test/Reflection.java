13422005d2d9a9e71c921d5d90d3c8b16ad181c6
hejing9599 <hejing9599@gmail.com>
2019-10-08 18:37:52 +0800

    第一次提交

---
+++ src/main/java/test/Reflection.java
@@ -0,0 +1,41 @@
+package test;
+
+/**
+ * Created by Qing on 2019/10/7.
+ */
+public class 反射 {
+
+    public static void main(String[] args) {
+
+        //第一种方法(不推荐,通过对象获取class)
+        Student stu1 = new Student();
+        Class<? extends Student> stuClass = stu1.getClass();
+
+        //第二种方法
+        Class stu = Student.class;
+
+        //第三种方法
+        try {
+            Class<?> stu2Class = Class.forName("test.Student");
+
+        } catch (ClassNotFoundException e) {
+
+
+        }
+
+
+    }
+}
+
+class Student {
+
+    private String name;
+
+    private Integer age;
+
+    public Student() {
+
+        //logic
+    }
+
+}
